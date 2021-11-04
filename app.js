5bb82a305323420065516adc204584481fcd9598
joshua360x <jkw5499@psu.edu>
2021-11-04 17:09:18 -0400

    Added feature of the header tell which animal is playing

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 // import functions and grab DOM elements
+const indicatorEl = document.getElementById('clickedOn');
+
+
 const catSoundEl = document.getElementById('cat');
 const dogSoundEl = document.getElementById('dog');
 const horseSoundEl = document.getElementById('horse');
@@ -18,12 +21,15 @@ const horseImgEl = document.getElementById('horseImg');
 
 catImgEl.addEventListener('click', () => {
     catSoundEl.play();
+    indicatorEl.textContent = 'You Clicked on the CAT';
 });
 
 dogImgEl.addEventListener('click', () => {
     dogSoundEl.play();
+    indicatorEl.textContent = 'You Clicked on the DOG';
 });
 
 horseImgEl.addEventListener('click', () => {
     horseSoundEl.play();
+    indicatorEl.textContent = 'You Clicked on the HORSE';
 });
